ec231fc79dad4cde2711228ab61c8013b4c0b294
Gustavo Barragan <dev@droni.co>
2025-01-28 13:51:15 -0500

    Fix: Menu

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,10 @@ export default defineNuxtConfig({
     head: {
       title: 'DevData',
       meta: [{ charset: 'utf-8' }],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/7.4.47/css/materialdesignicons.min.css' },
+      ],
     },
   },
 })
