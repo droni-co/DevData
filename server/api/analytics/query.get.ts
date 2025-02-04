f840022537f1e832ab69927b208539fc2eeb9507
Gustavo Barragan <dev@droni.co>
2025-02-04 08:53:18 -0500

    Feat: model Log

--- server/api/analytics/query.get.ts
+++ server/api/analytics/query.get.ts
@@ -1,13 +1,25 @@
 import { AzureQueryService } from "../../../services/AzureQuery.service";
 import { Durations } from "@azure/monitor-query";
 
-export default defineEventHandler(async (event) => {
-  const logsClient = await AzureQueryService.logsClient()
-  const resourceId = '/subscriptions/4271a21d-b0b3-4618-b735-d109ef5c64fe/resourceGroups/RSGR-DS-NNFF-001/providers/Microsoft.Web/sites/APSV-DS-NNFF-SSS-003'
-  const records = await logsClient.queryResource(resourceId, 'AppServiceConsoleLogs', { duration: Durations.oneHour }, {
-    serverTimeoutInSeconds: 600,
-    includeQueryStatistics: true,
-  })
+const query = async () => {
+  try {
+    const logsClient = await AzureQueryService.logsClient()
+    const workspaceId = String(process.env.WORKSPACE_ID)
+    const query = `AppServiceConsoleLogs
+      | where _ResourceId contains "APSV-PR"
+      | where Level == "Error"
+      | project TimeGenerated, Level, ResultDescription, _ResourceId`
+    const kustoQuery = "AppServiceConsoleLogs";
+    const records = await logsClient.queryWorkspace(workspaceId, kustoQuery, { duration: Durations.oneHour })
+    return records
+  } catch (error) {
+    console.error(error)
+    return null
+  }  
+}
 
-  return records
+export default defineEventHandler(async (event) => {
+  
+  const result = await query()
+  return { hola: 'hola' }
 })
